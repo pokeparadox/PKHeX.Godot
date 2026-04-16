bc4f20feda3bab8af568b827e29eb28aa80a15b2
PokeParadox <pokeparadox@gmail.com>
2026-04-16 23:05:39 +0100

    -Initial theming ideas -Tweaking layout

--- controls/pkm_view.gd
+++ controls/pkm_view.gd
@@ -1,4 +1,4 @@
-extends VBoxContainer
+extends PanelContainer
 
 const PkmDisplay = preload("res://controls/pkm_display.tscn")
 
@@ -26,7 +26,7 @@ func load_pkm_listing(save_hash : String, loctn : LOCATION, index : int = 0) -> 
 			load_server_pokemon(location_index)
 
 func load_party_pkm(save_hash : String) -> void:
-	$BoxName.text = "Party"
+	%BoxName.text = "Party"
 	var party: Array[PkmDisplayModel] = await PkHexRest.get_pkm_party_display_listing(save_hash)
 	var dump : Array[String] = await PkHexRest.dump_party_pkm(save_hash)
 	if dump.size() == party.size():
@@ -42,9 +42,9 @@ func load_server_pokemon(page: int, num_pkm: int = 10) -> void:
 	pass
 	
 func load_pkm(pkm : Array[PkmDisplayModel], save_hash : String) -> void:
-	for child in $PKMListing.get_children():
+	for child in %PKMListing.get_children():
 		child.queue_free()
 	for model in pkm:
 		var pkm_scn: Node = PkmDisplay.instantiate()
 		await pkm_scn.setup(model, save_hash);
-		$PKMListing.add_child(pkm_scn)
+		%PKMListing.add_child(pkm_scn)
